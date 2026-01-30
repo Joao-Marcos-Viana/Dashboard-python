0d36f35462a00db1123e4c7d679225eba1600c6e
João Marcos Viana <joaomarcosvianadias@gmail.com>
2026-01-30 16:20:24 -0300

    Adicionado comentarios e melhorias esteticas simples

--- dashboard.py
+++ dashboard.py
@@ -6,6 +6,7 @@ from supabase import create_client, Client
 from dotenv import load_dotenv
 import os
 #---
+st.set_page_config(page_title="Dashboard - Karaberu", layout="wide")
 
 load_dotenv()
 
@@ -20,22 +21,25 @@ df = pd.DataFrame(response.data)
 if not df.empty:
     
     #Garantindo que os campos numéricos estão no formato correto
-    df['preço de venda'] = pd.to_numeric(df['price']).fillna(0)
-    df['custo de produção'] = pd.to_numeric(df['cost']).fillna(0)
+    df['price'] = pd.to_numeric(df['price']).fillna(0)
+    df['cost'] = pd.to_numeric(df['cost']).fillna(0)
     
     #Cálculo do lucro
     df['profit'] = df['price'] - df['cost']
     
+    #Apresentação do Dashboard
     st.title("Dashboard de Projetos - Karaberu")
     st.markdown("Análise dos projetos cadastrados na plataforma Karaberu.")
     st.title("")
     total_vendas = df[df['status'] == 'vendido']['price'].sum()
     total_lucro = df['profit'].sum()
 
+    # Métricas principais
     col1, col2 = st.columns(2)
     col1.metric("Total Vendido", f"R$ {total_vendas:,.2f}")
     col2.metric("Lucro Total", f"R$ {total_lucro:,.2f}")
 
+    # Gráficos
     df_status = df['status'].value_counts().reset_index()
     df_status.columns = ['status', 'quantidade']
 
@@ -51,8 +55,9 @@ if not df.empty:
     fig_comp = px.bar(
         df, 
         x='name', 
-        y=['preço de venda', 'custo de produção'], 
+        y=['price', 'cost'], 
         title='Preço vs Custo por Projeto',
+        labels={'value':'Valor (R$)', 'name':'Projetos', "variable": "Legenda"},
         barmode='group' 
     )
     st.plotly_chart(fig_comp)
